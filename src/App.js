d50e1543ec77aa587157919efbd23c1c1c0240ab
RickyDLong <ricky.long.junior@hotmail.com>
2023-03-27 12:56:48 -0500

    Minor changes to apply.jsx

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Committee from './components/Committee';
+import Apply from './components/Apply';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />  
       <Hero />
       <Committee />
+      <Apply />
     </div>
   );
 }
---
+++ src/components/Apply.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Apply = () => {
+  return (
+    <div className='w-full py-16 text-ulm_maroon'>
+        <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
+            <div className='lg:col-span-2'>
+                <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>
+                How to apply to the committee?</h1>
+                <p></p>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Apply
